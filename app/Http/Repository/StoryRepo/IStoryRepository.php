7bbed55ff687499a346e3589657014d9ef23419b
mbaqes <mbaqes>
2022-08-26 22:15:10 +0200

    Story Repository Impl

---
+++ app/Http/Repository/StoryRepo/IStoryRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Interfaces;
+
+interface StoryRepositoryInterface 
+{
+    public function getAllStory();
+    public function getStoryById();
+    
+}
---
+++ app/Http/Repository/StoryRepo/StoryRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Repository;
+
+ 
+use App\Interfaces\StoryRepositoryInterface;
+ 
+
+class StoryRepository implements StoryRepositoryInterface 
+{
+    public function getAllStory() 
+    {
+        return  ;
+    }
+    public function getStoryById() 
+    {
+        return  ;
+    }
+}
